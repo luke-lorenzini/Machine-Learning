90925acfbc29449e0831062af10427cd437f3187
Luke Lorenzini <luke.lorenzini@gmail.com>
2017-01-15 09:54:50 +0000

    Create mean_normalization.py

---
+++ mean_normalization.py
@@ -0,0 +1,12 @@
+# Mean Normalization
+def meannorm(x):
+    import numpy
+
+    a = x
+    b = numpy.average(a, axis=0)
+    c = numpy.std(a, axis=0)
+
+    # myx = x / x.max(axis=0)
+    myx = (a - b) / c
+
+    return myx
