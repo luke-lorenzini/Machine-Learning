af5f48e64df3e05cfa01a1f7e12979f8397f88b2
Luke Lorenzini <luke.lorenzini@gmail.com>
2017-02-19 09:23:06 +0000

    File rename.  Reorganize code

---
+++ neural_network.py
@@ -0,0 +1,182 @@
+def NeuralNet(x, y):
+    import numpy
+    import numpy.matlib
+    from helpers import init_weights
+    from helpers import append
+    from helpers import calc_z
+    from helpers import sigmoid
+    from helpers import sigmoid_prime
+    from helpers import calc_alpha
+
+    # mode = 0, batch gradient descent
+    # mode = 1, vectorized batch gradient descent
+    # mode = 2, stochastic gradient descent
+    # mode = 3, mini-batch gradient descent
+    mode = 2
+
+    # Modify learning rate, alpha
+    alpha = 1
+    const1 = 1
+    const2 = 5
+
+    # Map Reduce
+    # like batch gradient descent, divide number of
+    # samples into N clusters, each 'machine' processes
+    # a cluster concurrently n1, n2...nN, clusters
+    # combined after,
+    # theta -= alpha(n1 + n2 + ... + nN)
+
+    # Set random seed for ease of testing
+    # numpy.random.seed(1)
+
+    myx = x.T
+    myy = y.T
+
+    epochs = 100
+    samples = myx.shape[1]
+    lamb = 0.001
+    bias = 0
+
+    # arch represents network architecture (w/o biases)
+    arch = (myx.shape[0], 2 * myx.shape[0], 2 * myy.shape[0], myy.shape[0])
+
+    ### Initialization ###
+    # Use an [mxn] matrix to create an [nx(n+1)]
+    theta1 = init_weights(arch[1], arch[0] + bias)
+    theta2 = init_weights(arch[3], arch[1] + bias)
+
+    d1_delta = numpy.zeros((theta1.shape[0], theta1.shape[1]))
+    d2_delta = numpy.zeros((theta2.shape[0], theta2.shape[1]))
+
+    def fwd_prop(mat):
+        """Forward Propogation"""
+        # Each column represents a training example
+        # Step 1, append the bias
+        act1 = mat
+        if bias > 0:
+            act1 = append(act1)
+
+        # Step 2, calculate z
+        z1 = calc_z(act1, theta1)
+
+        # Step 3, transform z
+        act2 = sigmoid(z1)
+
+        # Second layer
+        # Step 1, append the bias
+        if bias > 0:
+            act2 = append(act2)
+
+        # Step 2, calculate z
+        z2 = calc_z(act2, theta2)
+
+        # Step 3, transform z
+        act3 = sigmoid(z2)
+
+        return act1, act2, act3
+
+    def back_prop(j, i, mat, act1, act2, act3):
+        """ Back Propogation"""
+        # Calculate errors for layer 3
+        # d3_error = act3 - myy[:, j]
+        d3_error = act3 - mat
+
+        if (i % (epochs / 10)) == 0:
+            if j == 0:
+                print(100 * numpy.mean(numpy.abs(d3_error)))
+
+        d2_error = numpy.multiply(numpy.dot(theta2.T, d3_error), sigmoid_prime(act2))
+
+        # Calculate errors for layer 2
+        if bias > 0:
+            d2_error = numpy.delete(d2_error, 0, 0)
+        d1_error = numpy.multiply(numpy.dot(theta1.T, d2_error), sigmoid_prime(act1))
+
+        return d1_error, d2_error, d3_error
+
+    for i in range(epochs):
+        alpha = calc_alpha(i, const1, const2)
+        if mode == 0: # batch gradient descent
+            for j in range(samples):
+                # Setup input / output data
+                in_mat = myx[:, j]
+                out_mat = myy[:, j]
+
+                # Forward Propogation
+                actvtn1, actvtn2, actvtn3 = fwd_prop(in_mat)
+
+                # Back Propogation
+                d1_error, d2_error, d3_error = back_prop(j, i, out_mat, actvtn1, actvtn2, actvtn3)
+
+                d2_delta += numpy.dot(d3_error, actvtn2.T)
+                d1_delta += numpy.dot(d2_error, actvtn1.T)
+
+            theta1 -= alpha * (d1_delta / samples + (lamb * theta1))
+            theta2 -= alpha * (d2_delta / samples + (lamb * theta2))
+
+        elif mode == 1: # vectorized batch gradient descent
+            # Setup input / output data
+            in_mat = myx
+            out_mat = myy
+
+            # Forward Propogation
+            actvtn1, actvtn2, actvtn3 = fwd_prop(in_mat)
+
+            # Back Propogation
+            d1_error, d2_error, d3_error = back_prop(0, i, out_mat, actvtn1, actvtn2, actvtn3)
+
+            d2_delta += numpy.dot(d3_error, actvtn2.T)
+            d1_delta += numpy.dot(d2_error, actvtn1.T)
+
+            theta1 -= alpha * (d1_delta / samples + (lamb * theta1))
+            theta2 -= alpha * (d2_delta / samples + (lamb * theta2))
+
+        elif mode == 2: # stochastic gradient descent
+            for j in range(samples):
+                # Setup input / output data
+                in_mat = myx[:, j]
+                out_mat = myy[:, j]
+
+                # Forward Propogation
+                actvtn1, actvtn2, actvtn3 = fwd_prop(in_mat)
+
+                # Back Propogation
+                d1_error, d2_error, d3_error = back_prop(j, i, out_mat, actvtn1, actvtn2, actvtn3)
+
+                d2_delta += numpy.dot(d3_error, actvtn2.T)
+                d1_delta += numpy.dot(d2_error, actvtn1.T)
+
+                theta1 -= alpha * (d1_delta / samples + (lamb * theta1))
+                theta2 -= alpha * (d2_delta / samples + (lamb * theta2))
+        elif mode == 3: # mini-batch gradient descent
+            for j in range(samples):
+                # Need to send a sub matrix of the entire set, e.g. 1-100, 101-200, 201-300
+                # Setup input / output data
+                in_mat = myx[:, j]
+                out_mat = myy[:, j]
+
+                # Forward Propogation
+                actvtn1, actvtn2, actvtn3 = fwd_prop(in_mat)
+
+                # Back Propogation
+                d1_error, d2_error, d3_error = back_prop(j, i, out_mat, actvtn1, actvtn2, actvtn3)
+
+                d2_delta += numpy.dot(d3_error, actvtn2.T)
+                d1_delta += numpy.dot(d2_error, actvtn1.T)
+
+            theta1 -= alpha * (d1_delta / samples + (lamb * theta1))
+            theta2 -= alpha * (d2_delta / samples + (lamb * theta2))
+
+    # Check the results by analyzing one training example (in this case 0)
+    check1 = myx
+    if bias > 0:
+        check1 = append(check1)
+    check2 = calc_z(check1, theta1)
+    check3 = sigmoid(check2)
+    if bias > 0:
+        check3 = append(check3)
+    check3 = calc_z(check3, theta2)
+    check4 = sigmoid(check3)
+    print(numpy.round(100 * check4))
+
+    return theta1, theta2
