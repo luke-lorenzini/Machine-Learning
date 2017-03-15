d5f528e3ac7429bc24127b4aabf23db99b50712c
Luke Lorenzini <luke.lorenzini@gmail.com>
2017-03-15 18:55:19 +0000

    Update script_wav.py

--- script_wav.py
+++ script_wav.py
@@ -9,9 +9,10 @@ from scipy import misc
 import wave
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
+import math
 
 # Read a wav file, convert it to L & R channels
-filename = r'Machine-Learning\Test Data\sound\wavTones.com.unregistred.sin_1000Hz_-6dBFS_3s.wav'
+filename = r'C:\Users\Luke Lorenzini\OneDrive\Visual Studio\Python\Machine-Learning\Test-Data\sound\wavTones.com.unregistred.sin_1000Hz_-6dBFS_3s.wav'
 # filename = r'Machine-Learning\Test Data\sound\folk.wav'
 ff = wave.open(filename, 'rb')
 print(ff.getparams())
@@ -38,13 +39,34 @@ for x in range(ff.getnframes()):
         valuesx[x] = np.int16(temp0)
 
 # One period = periods * sample rate / frequency, sample file, 1kHz --> 2 * 44,100 / 1000 
-plt.plot(valuesx)
+# plt.plot(valuesx)
+# plt.show()
+
+n = len(valuesx)
+p = np.fft.fft(valuesx[:, 0])
+
+uniquePts = int(math.ceil((n+1)/2.0))
+p = p[0:uniquePts]
+p = abs(p)
+
+p = p / float(n)
+p = p**2
+
+if n % 2 > 0:
+    p[1:len(p)] = p[1:len(p)] * 2
+else:
+    p[1:len(p) - 1] = p[1:len(p) - 1] * 2
+
+sampFreq = ff.getframerate()
+freqArray = np.arange(0, uniquePts, 1.0) * (sampFreq / n)
+
+plt.plot(freqArray / 1000, 10 * np.log10(p))
 plt.show()
 
-# valuesx = np.fft.fft(valuesx)
-# d = len(c)/2
-# plt.plot(abs(c[:(d-1)]), 'r')
-# plt.show()
+valuesx_f = np.fft.fft(valuesx[:, 0])
+d = len(valuesx_f)/2
+plt.plot(abs(valuesx_f[:(d-1)]), 'r')
+plt.show()
 
 valuesy = valuesx[1:valuesx.shape[0], :]
 valuesx = valuesx[0:valuesx.shape[0] - 1, :]
