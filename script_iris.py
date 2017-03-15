f8c7d14b1e18abc53750c38e2edec8088261433a
Luke Lorenzini <luke.lorenzini@gmail.com>
2017-03-15 18:53:14 +0000

    Update script_iris.py

--- script_iris.py
+++ script_iris.py
@@ -1,4 +1,4 @@
-# A Script to test concepts
+"""A Script to test concepts"""
 from time import time
 import numpy as np
 from ML.PCA import prince
@@ -7,25 +7,29 @@ from ML.neural_network import NeuralNet
 from ML.k_means import kmeans
 from ML.one_hot import one_hot
 
-filename = r'Machine-Learning\Test Data\iris\irisNN.csv'
-with open(filename, newline='') as csvfile:
+# The compression rate for PCA
+RATE = 99
+
+# filename = r'Machine-Learning\Test-Data\iris\irisNN.csv'
+FILENAME = r'C:\Users\Luke\OneDrive\Visual Studio\Python\Machine-Learning\Test-Data\iris\irisNN.csv'
+with open(FILENAME, newline='') as csvfile:
     valuesx = np.matrix(np.loadtxt(csvfile, delimiter=",", usecols=(0, 1, 2, 3)))
-with open(filename, newline='') as csvfile:
+with open(FILENAME, newline='') as csvfile:
     valuesy = np.matrix(np.loadtxt(csvfile, delimiter=",", usecols=(5, 6, 7)))
 
 valuesz = valuesx
 
-valuesz = prince(valuesz)
+# valuesz = prince(valuesz, RATE)
 valuesz = meannorm(valuesz)
 
-clusters = 3
-timenow = time()
-valuesy = one_hot(kmeans(valuesz, clusters), clusters)
-print(time() - timenow)
+# CLUSTERS = 3
+# TIMENOW = time()
+# valuesy = one_hot(kmeans(valuesz, CLUSTERS), CLUSTERS)
+# print(time() - TIMENOW)
 
-timenow = time()
+TIMENOW = time()
 t1, t2 = NeuralNet(valuesz, valuesy)
-print(time() - timenow)
+print(time() - TIMENOW)
 
 # np.savetxt("theta1.csv", t1, delimiter=",")
 # np.savetxt("theta2.csv", t2, delimiter=",")
