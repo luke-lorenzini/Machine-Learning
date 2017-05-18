1e2f1c7a44e86eff21fa1aca7f6b911a644db3b6
Luke Lorenzini <luke.lorenzini@gmail.com>
2017-05-18 11:00:30 +0100

    Interpret incoming text file to one-hot

--- script_RNN.py
+++ script_RNN.py
@@ -1,5 +1,25 @@
 from ML.RNN import RNN
+import numpy as np
 
-o, s = RNN()
+WORDS = 128
 
-print(o, s)
+FILENAME = r'..\Machine-Learning\Test-Data\Sample Text\sample.txt'
+
+f = open(FILENAME, 'r')
+x = f.read()
+
+in_data = {}
+
+# Our input data
+# in_data = np.zeros((len(x), WORDS))
+
+for i in range(len(x)):
+    in_data[i] = np.zeros((WORDS, 1))
+    myChar = ord(x[i])
+
+    # # Convert into one hot
+    in_data[i][myChar] = 1
+
+o = RNN(in_data, WORDS)
+
+print(o)
